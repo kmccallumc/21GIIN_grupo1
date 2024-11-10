fc575d7d8f49fdedadb1b080260436cc8ad08a9e
kmcca <kmcca@Kathleen-NewHP>
2024-11-10 17:31:47 +0100

    21GIIN-Grupo1-Cafe

---
+++ cafeGrupo1/src/main/java/com/viu/cafegrupo1/CafeGrupo1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.viu.cafegrupo1;
+
+/**
+ *
+ * @author kmccallum <Kathleen_McCallum VIU>
+ * @author dcollado <David Collado VIU>
+ * @author oguardia <Oscar Guardia VIU>
+ */
+public class CafeGrupo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, esta es la Empresa de Cafe Grupo1!");
+    }
+}
