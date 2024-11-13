53d7ecb25c7cccf045115134ea440e15135d241d
kmcca <kmcca@Kathleen-NewHP>
2024-11-13 20:38:56 +0100

    21GIIN-Grupo1-Cafe. Implementacion Empleado y Producto

--- cafeGrupo1/src/main/java/com/viu/cafegrupo1/Empleado.java
+++ cafeGrupo1/src/main/java/com/viu/cafegrupo1/Empleado.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.viu.cafegrupo1;
-
 /**
  *
  * @author kmccallum <Kathleen_McCallum VIU>
+ * 
  */
+
+package com.viu.cafegrupo1;
+
 public class Empleado {
+    String nombres;
+    String apellidos;
+    String documento_id;
+    double salario;
     
+    // el constructor
+    public Empleado(String nombres, String apellidos, String documento_id, Double salario){
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.documento_id = documento_id;
+        this.salario = salario;
+    }
+    
+    // los gets
+    public String getNombres(){
+        return this.nombres;
+    }
+    public String getApellidos(){
+        return this.apellidos;
+    }
+    public String getDocumentoId(){
+        return this.documento_id;
+    }
+    public Double getSalario(){
+        return this.salario;
+    }
+    
+    // los sets
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public void setDocumentoId(String doc_id) {
+        this.documento_id = doc_id;
+    }
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
 }
