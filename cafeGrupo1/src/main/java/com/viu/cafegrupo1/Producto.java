d3afb4abbcf2e0ceebf318c16743ffe64e4f7a1c
kmcca <kmcca@Kathleen-NewHP>
2024-11-13 20:37:00 +0100

    21GIIN-Grupo1-Cafe. Implementacion Empleado y Producto

--- cafeGrupo1/src/main/java/com/viu/cafegrupo1/Producto.java
+++ cafeGrupo1/src/main/java/com/viu/cafegrupo1/Producto.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.viu.cafegrupo1;
-
 /**
  *
  * @author kmccallum <Kathleen_McCallum VIU>
  */
+
+package com.viu.cafegrupo1;
+
 public class Producto {
+    String nombreProducto;
+    Double precio;
+    int stock;
     
+    public Producto (String nomProd, Double precio, int stock){
+        this.nombreProducto = nomProd;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    
+    // los gets
+    
+    public String getNombreProducto(){
+        return this.nombreProducto;
+    }
+    public double getPrecio(){
+        return this.precio;
+    }
+    public int getStock(){
+        return this.stock;
+    }
+    
+    // los sets
+    
+    public void setNombreProducto(String nomprod){
+        this.nombreProducto = nomprod;
+    }
+    
+    public void setPrecio(Double precio){
+        this.precio = precio;
+    }
+    
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+    
+    // Valor total del inventario
+    public Double valorTotalInventario(){
+        return (this.stock * this.precio);
+    }
 }
