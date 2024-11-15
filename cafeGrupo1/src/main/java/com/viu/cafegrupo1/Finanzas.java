d6bb7ef617dff27fa2b06b4ec28537a8de97fa4e
kmccallum <kmccallumc@hotmail.com>
2024-11-15 21:01:16 +0100

    21GIIN-Grupo1-Cafe. Implementacion encapsulamiento

--- cafeGrupo1/src/main/java/com/viu/cafegrupo1/Finanzas.java
+++ cafeGrupo1/src/main/java/com/viu/cafegrupo1/Finanzas.java
@@ -8,6 +8,32 @@ package com.viu.cafegrupo1;
  *
  * @author kmccallum <Kathleen_McCallum VIU>
  */
-public class Finanzas {
+  class Finanzas {
+     private double ingresos;
+     private double gastos;
+     
+     public Finanzas(double ingresos, double gastos){
+         this.ingresos = ingresos;
+         this.gastos = gastos;
+     }
     
+     // sets y gets de Ingresos
+     public double getIngresos(){
+         return this.ingresos;
+     }
+     public void setIngresos(double ingreso){
+         this.ingresos = ingreso;
+     }
+     
+     // set y get de gastos
+     public double getGastos(){
+         return this.gastos;
+     }
+     public void setGastos(double gastos){
+         this.gastos = gastos;
+     }
+     
+     public double beneficioNeto(){
+         return (this.ingresos - this.gastos);
+     }
 }
